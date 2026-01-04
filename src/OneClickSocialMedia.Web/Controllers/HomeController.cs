01881371414e019ab15d666f73535b29b48e3d53
Ibrahim <ibbyi7i@yahoo.com>
2026-01-04 11:35:59 +0000

    Added views for homepage and settings

--- src/OneClickSocialMedia.Web/Controllers/HomeController.cs
+++ src/OneClickSocialMedia.Web/Controllers/HomeController.cs
@@ -16,6 +16,11 @@ namespace OneClickSocialMedia.Controllers
             return View();
         }
 
+        public IActionResult Setting()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
