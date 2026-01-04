22d2fcb922bcfba61ec4ae746478fbd1355456b2
Ibrahim <ibbyi7i@yahoo.com>
2026-01-04 13:59:35 +0000

    added anti forgey token to deal with potiental cross site request forgery

--- src/OneClickSocialMedia.Web/Controllers/HomeController.cs
+++ src/OneClickSocialMedia.Web/Controllers/HomeController.cs
@@ -22,6 +22,7 @@ namespace OneClickSocialMedia.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult PostToSocialMedia()
         {
             return View();
