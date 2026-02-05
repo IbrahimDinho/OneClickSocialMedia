1dfa5e20d1acca356929d3a858f8127274075f2b
Ibrahim <ibbyi7i@yahoo.com>
2026-02-05 01:42:42 +0000

    add hide and show on settings for twitter settings

---
+++ src/OneClickSocialMedia.Web/wwwroot/js/setting-page.js
@@ -0,0 +1,17 @@
+﻿function togglePassword(inputId, btn) {
+    const input = document.getElementById(inputId);
+    const icon = btn.querySelector("i");
+    const text = btn.querySelector(".toggle-text");
+
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+        text.innerText = "Hide";
+    } else {
+        input.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+        text.innerText = "Show";
+    }
+}
