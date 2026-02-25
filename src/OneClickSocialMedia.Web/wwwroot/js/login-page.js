d29b19fe3da0df0d25f2ce76b887dadf843f893f
Ibrahim <ibbyi7i@yahoo.com>
2026-02-25 22:53:13 +0000

    Added show/hide for forgot password in login page

---
+++ src/OneClickSocialMedia.Web/wwwroot/js/login-page.js
@@ -0,0 +1,21 @@
+﻿function togglePassword(inputId, btn) {
+    const input = document.getElementById(inputId);
+    const icon = btn.querySelector("i");
+    const text = btn.querySelector(".toggle-text");
+
+    if (!input.value || input.value.trim() === "") {
+        return;
+    }
+
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+        text.innerText = "Hide";
+    } else {
+        input.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+        text.innerText = "Show";
+    }
+}
