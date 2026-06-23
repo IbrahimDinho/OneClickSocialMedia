7cd31736d436b85ac43e5b644e8e7f02afda80b1
Ibrahim <ibbyi7i@yahoo.com>
2026-06-24 00:45:59 +0100

    allow for structured logging through overload

--- src/LoggingService/ILoggerManager.cs
+++ src/LoggingService/ILoggerManager.cs
@@ -6,5 +6,10 @@
         void LogInformation(string message);
         void LogWarning(string message);
         void LogError(string message);
+
+        void LogDebug(string messageTemplate, params object[] propertyValues);
+        void LogInformation(string messageTemplate, params object[] propertyValues);
+        void LogWarning(string messageTemplate, params object[] propertyValues);
+        void LogError(string messageTemplate, params object[] propertyValues);
     }
 }
--- src/LoggingService/LoggerManager.cs
+++ src/LoggingService/LoggerManager.cs
@@ -23,5 +23,26 @@ namespace LoggingService
         {
             logger.Warning(message);
         }
+
+        public void LogDebug(string messageTemplate, params object[] propertyValues)
+        {
+            logger.Debug(messageTemplate, propertyValues);
+        }
+
+        public void LogInformation(string messageTemplate, params object[] propertyValues)
+        {
+            logger.Information(messageTemplate, propertyValues);
+        }
+
+        public void LogWarning(string messageTemplate, params object[] propertyValues)
+        {
+            logger.Warning(messageTemplate, propertyValues);
+        }
+
+        public void LogError(string messageTemplate, params object[] propertyValues)
+        {
+            logger.Error(messageTemplate, propertyValues);
+        }
+
     }
 }
